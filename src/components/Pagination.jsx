60176ae6756e942058e7424edca7ee87edb0943a
Shawn Diemart <onyxiakiller13@gmail.com>
2024-06-24 14:35:47 -0400

    pagination component

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Pagination as BSPagination } from 'react-bootstrap';
+
+const Pagination = ({ currentPage, pageSize, totalItems, onChange }) => {
+    const totalPages = Math.ceil(totalItems / pageSize);
+
+    return (
+        <BSPagination>
+            {[...Array(totalPages)].map((_, index) => (
+                <BSPagination.Item
+                    key={index + 1}
+                    active={(index = 1 === currentPage)}
+                    onClick={() => onChange(index + 1)}>
+                    {index + 1}
+                </BSPagination.Item>
+            ))}
+        </BSPagination>
+    );
+};
+
+export default Pagination;
